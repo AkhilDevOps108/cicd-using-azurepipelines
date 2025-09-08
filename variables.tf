c645cef093c266438f2e4bd14670d3314dd24000
Akhil kumar <154546670+AkhilDevOps108@users.noreply.github.com>
2025-09-08 18:30:57 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "rg_name" {
+  type    = string
+  default = "demo-rg-tf"
+}
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+variable "storage_prefix" {
+  type    = string
+  default = "demostg"
+}
